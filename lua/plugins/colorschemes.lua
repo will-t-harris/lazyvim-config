93cf21d5272cc87d29cb2300a91be15ccfe85f90
Will Harris <will.t.harris@protonmail.com>
2024-10-30 14:23:00 -0700

    initial config

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  "rose-pine/neovim",
+  "embark-theme/vim",
+  "ajmwagar/vim-deus",
+  "Yazeed1s/oh-lucy.nvim",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "embark",
+    },
+  },
+}
